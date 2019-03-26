4bc44fd2d0edd0f4d7452179675653182a2ff79e
Murat Kumykov <mkumykov@blackducksoftware.com>
2019-03-26 09:17:13 -0400

    .

--- docker-swarm/better_postgresql_dump_backup.sh
+++ docker-swarm/better_postgresql_dump_backup.sh
@@ -2,49 +2,51 @@
 #
 # DOCKER compatible backup script. Works with single node docker swarm or docker compose
 #
-  
+
 timestamp=$(date +%Y%m%d_%H_%M_%S)
 prefix=$1
 retainqty=3
-  
+
 # Location to place the backup files
 backup_dir="/opt/hubbackupdumps/"
- 
+
 # Log file location.
 logfile="/opt/hubbackupdumps/hub_pgdump.log"
- 
- 
+
+# Databases to back up
+dblist="bds_hub bdio bds_hub_report"
+
+
 if [ "$1" = "" ]
 then
         prefix=daily
 fi
-  
+
 #Finding the container id
 container_id=$(docker container ls | grep -E '(hub-postgres|blackduck-postgres)' | awk '{print $1}')
-  
+
 if [ $container_id != "" ]
 then
-        echo "Backup started at $(date +%Y%m%d_%H_%M_%S)  for bds_hub database" >> $logfile
-        /bin/docker exec -i $container_id  pg_dump -U blackduck -Fc -f /tmp/bds_hub.dump bds_hub
+    for i in $dblist
+    do
+        dumpfile=${backup_dir}/${prefix}_${i}_${timestamp}.dump
+        echo "Backup started at $(date +%Y%m%d_%H_%M_%S)  for ${i} database" >> $logfile
+        /bin/docker exec -i $container_id  pg_dump -U blackduck -Fc -f ${i}  >${dumpfile}
         if [ $? == 0 ]
         then
-                /bin/docker cp $container_id:/tmp/bds_hub.dump $backup_dir/${prefix}_bds_hub_$timestamp.dump
-                echo -e "Backup completed at $(date +%Y%m%d_%H_%M_%S) for bds_hub database \n" >> $logfile
+            echo -e "Backup completed at $(date +%Y%m%d_%H_%M_%S) for bds_hub database \n" >> $logfile
         else
-                echo -e "Backup  failed at $(date +%Y%m%d_%H_%M_%S) \n" >> $logfile
-                exit 1
+            echo -e "Backup  failed at $(date +%Y%m%d_%H_%M_%S) \n" >> $logfile
+            exit 1
         fi
+        dumpfilepattern=${backup_dir}/${prefix}_${i}_[0-9]
+        dumpcount=$(ls ${dumpfilepattern}* | wc -l)
+        if [ $dumpcount -gt $retainqty ]
+        then
+          rm $(ls -r ${dumpfilepattern}* | tail -n $(( $dumpcount - $retainqty )) )
+        fi
+    done
 else
         echo -e "Container id not found at $(date +%Y%m%d_%H_%M_%S) \n" >> $logfile
         exit 1
-fi
-  
-#find "$backup_dir/" -mtime +20 -name bds_hub_*.dump -exec rm {} \;
-  
-# Leaving only the last $retainqty backup files for backup $prefix
-  
-dumpcount=$(ls $backup_dir/${prefix}* | wc -l)
-if [ $dumpcount -gt $retainqty ]
-then
-        rm $(ls -r $backup_dir/${prefix}* | tail -n $(( $dumpcount - $retainqty )) )
 fi
