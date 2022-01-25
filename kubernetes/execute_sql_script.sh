ebd2eca30d6897ecb56edc30168b6fc4c41b137c
Murat Kumykov <kumykov@snps-mmhqia9idfdi.home>
2022-01-25 15:13:49 -0500

    added utility scripts

---
+++ kubernetes/execute_sql_script.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# KUBERNETES compatible backup script.
+# requires to be run in an environment with working kubectl
+
+if [ "$3" = "" ]
+then
+  echo
+  echo usage: $0 namespace database dumpfile
+  echo
+  exit 1
+fi
+
+namespace=$1
+database=$2
+sqlfile=$3
+
+echo "Restore process started for $dumpfile into $database "
+echo Namespace: $namespace
+
+#Finding the container id
+#container_id=$(docker container ls | grep hub-postgres:4.1.0 | awk '{print $1}')
+container_id=$(kubectl get pods -n $namespace | awk '/postgres/ {print $1}')
+
+if [ $container_id != "" ]
+then
+  echo "Processing $sqlfile into $database " 
+  cat "${sqlfile}" | kubectl exec -i -n ${namespace} ${container_id} -- psql -d ${database} 
+fi
